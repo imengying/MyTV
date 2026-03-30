787a151f7a61b5087372e31e5ec38a01946b197f
imengying <yuzucv@gmail.com>
2026-03-31 01:30:37 +0800

    Fixes and improvements

---
+++ src/lib/admin.types.ts
@@ -0,0 +1,53 @@
+export interface AdminConfig {
+  ConfigSubscribtion: {
+    URL: string;
+    AutoUpdate: boolean;
+    LastCheck: string;
+  };
+  ConfigFile: string;
+  SiteConfig: {
+    SiteName: string;
+    Announcement: string;
+    SearchDownstreamMaxPage: number;
+    SiteInterfaceCacheTime: number;
+    DoubanProxyType: string;
+    DoubanProxy: string;
+    DoubanImageProxyType: string;
+    DoubanImageProxy: string;
+    DisableYellowFilter: boolean;
+    FluidSearch: boolean;
+  };
+  UserConfig: {
+    Users: {
+      username: string;
+      role: 'user' | 'admin' | 'owner';
+      banned?: boolean;
+      enabledApis?: string[]; // 优先级高于tags限制
+      tags?: string[]; // 多 tags 取并集限制
+    }[];
+    Tags?: {
+      name: string;
+      enabledApis: string[];
+    }[];
+  };
+  SourceConfig: {
+    key: string;
+    name: string;
+    api: string;
+    detail?: string;
+    from: 'config' | 'custom';
+    disabled?: boolean;
+  }[];
+  CustomCategories: {
+    name?: string;
+    type: 'movie' | 'tv';
+    query: string;
+    from: 'config' | 'custom';
+    disabled?: boolean;
+  }[];
+}
+
+export interface AdminConfigResult {
+  Role: 'owner' | 'admin';
+  Config: AdminConfig;
+}
